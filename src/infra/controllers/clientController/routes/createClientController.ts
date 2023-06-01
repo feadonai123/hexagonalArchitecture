f412775e1285ff2afde9f0e53bbb5c9f20ee29f2
feadonai123 <felipe.2002@alunos.utfpr.edu.br>
2023-06-01 10:44:05 -0300

    Add DTO to controllers

--- src/infra/controllers/clientController/routes/createClientController.ts
+++ src/infra/controllers/clientController/routes/createClientController.ts
@@ -1,29 +1,32 @@
 import { Request, Response } from 'express';
 import { ClientUseCasePort } from '@/domain/useCases/clientUseCase/clientUseCasePort';
+import { ICreateClientData } from '@/domain/useCases/clientUseCase/clientUseCasePort';
 
 interface ICreateClientController {
   listenForRequest(req: Request, res: Response): Promise<void>
+}
+interface DTO {
+  name: string
+  email: string
+  phone: string
+  cpf: string
+  birthDate: string
 }
 
 export const createClientController = (clientUseCase: ClientUseCasePort) : ICreateClientController =>{
   
   return {
     async listenForRequest (req: Request, res: Response): Promise<void> {
-      const {
-        name,
-        email,
-        phone,
-        cpf,
-        birthDate
-      } = req.body;
-    
-      const client = await clientUseCase.createClient({
-        name,
-        email,
-        phone,
-        cpf,
-        birthDate: new Date(birthDate)
-      })
+      const requestData : DTO = req.body;
+      const clientCreate : ICreateClientData= {
+        name: requestData.name,
+        email: requestData.email,
+        phone: requestData.phone,
+        cpf: requestData.cpf,
+        birthDate: new Date(requestData.birthDate)
+      }
+
+      const client = await clientUseCase.createClient(clientCreate)
     
       res.status(201).json({
         clientId: client.id
